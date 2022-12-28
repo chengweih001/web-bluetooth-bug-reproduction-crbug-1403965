04fda2112a697b823fbdae0e8249e312784e6d99
Adnane Hamid <adnane.hamid.20@gmail.com>
2022-12-28 12:34:42 +0100

    allow connection to any bluetooth device

--- log-helper.js
+++ log-helper.js
@@ -13,27 +13,15 @@ var ChromeSamples = {
   clearLog: function () {
     document.querySelector('#log').textContent = '';
   },
-
-  setStatus: function (status) {
-    document.querySelector('#status').textContent = status;
-  },
-
-  setContent: function (newContent) {
-    var content = document.querySelector('#content');
-    while (content.hasChildNodes()) {
-      content.removeChild(content.lastChild);
-    }
-    content.appendChild(newContent);
-  },
 };
 
 log = ChromeSamples.log;
+clearLog = ChromeSamples.clearLog
 
 function isWebBluetoothEnabled() {
   if (navigator.bluetooth) {
     return true;
   } else {
-    ChromeSamples.setStatus('Web Bluetooth API is not available.\n' + 'Please make sure the "Experimental Web Platform features" flag is enabled.');
     return false;
   }
 }
--- watch-advertisements-and-connect.js
+++ watch-advertisements-and-connect.js
@@ -6,6 +6,12 @@ function onRequestBluetoothDeviceButtonClick() {
     })
     .then((device) => {
       log('> Requested ' + device.name);
+      const connectButton = document.createElement('button');
+      connectButton.textContent = device.name;
+      connectButton.addEventListener('click', function() {
+        onConnectToDeviceButtonClick(device.name);
+      })
+      document.querySelector('#connectButtons').appendChild(connectButton)
     })
     .catch((error) => {
       log('Argh! ' + error);
@@ -19,10 +25,10 @@ function onConnectToDeviceButtonClick(deviceName) {
     .then((devices) => {
       const device = devices.find((pairedDevice) => pairedDevice.name === deviceName);
       if (device) {
-        log('found an authorized device, attempting connection...');
+        log(`found an authorized device name ${deviceName}, attempting connection...`);
         device.gatt.connect().then(() => console.log('connected'));
       } else {
-        log("Argh! no authorized device was found, please authorize it using the 'request device' button");
+        log("Argh! no authorized device was found with this name");
       }
     })
     .catch((error) => {
